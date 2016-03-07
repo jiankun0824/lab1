0ef5314509d4e99a9fb3a2121239c193f80d75f3
jiankun0824 <F74045076@mail.ncku.edu.tw>
2016-03-07 19:58:29 +0800

    seg.fault

---
+++ lab1.cpp
@@ -0,0 +1,29 @@
+#include<cstdio>
+using namespace std;
+void rcs(int a)
+{
+	if(a==1)
+	{
+		printf("%d\n",a);
+		return;
+		}
+	if((a%2)==0)
+	{
+		printf("%d\n",a);
+		rcs(a/2);
+		}
+	if((a%2)==1)
+		printf("%d\n",a);
+		rcs(3*a+1);
+	}
+
+int main()
+{
+	int question;
+	scanf("%d",&question);
+
+	rcs(question);
+	return 0;
+	}
+
+
